eaf36c860924b879046a5f4526be7872759cc22d
Stephen Puthenpurackal <datfoosteve@gmail.com>
2022-11-14 07:14:31 -0600

    yep

--- metalweightcalculator/app/notes/page.tsx
+++ metalweightcalculator/app/notes/page.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link";
+import styles from './Notes.module.css';
+import CreateNote from './CreateNote';
 
 async function getNotes() {
-  const res = await fetch('http://127.0.0.1:8090/api/collections/notes/records?page=1&per_page=10');
+  const res = await fetch('http://127.0.0.1:8090/api/collections/notes/records?page=1&per_page=10', { cache: 'no-store' });
     const data = await res.json();
     return data?.items as any[];
 
@@ -9,32 +11,31 @@ async function getNotes() {
 
 export default async function NotesPage() {
     const notes = await getNotes();
-  return (
-    <div>
-      <h1>Notes Page</h1>
+  
+    return(
       <div>
-        {notes.map((note) => {
-            return (
-                <div key={note.id}>
-                <h2>{note.title}</h2>
-                <p>{note.content}</p>
-                </div>
-            );
-        } )}
+        <h1>Notes</h1>
+        <div className={styles.grid}>
+          {notes?.map((note) => {
+            return <Note key={note.id} note={note} />;
+          })}
         </div>
+  
+        <CreateNote />
       </div>
-  );
-}
-
-function Note({ note }: any) {
+    );
+  }
+  
+  function Note({ note }: any) {
     const { id, title, content, created } = note || {};
-
+  
     return (
-        <Link href={`/notes/${id}`}>
-            <div>
-                <h2>{title}</h2>
-                <p>{content}</p>
-                <p>{created}</p>
-            </div>
-        </Link>
-);};
+      <Link href={`/notes/${id}`}>
+        <div className={styles.note}>
+          <h2>{title}</h2>
+          <h5>{content}</h5>
+          <p>{created}</p>
+        </div>
+      </Link>
+    );
+  }
--- metalweightcalculator/app/page.tsx
+++ metalweightcalculator/app/page.tsx
@@ -2,7 +2,7 @@ export default function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
-      <p>Some content</p>
+      <p>Notes</p>
     </div>
   );
 }
